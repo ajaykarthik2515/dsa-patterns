469d09b9e293aa8755fc85e42b876cd0cfd11a18
ajaykarthik2515 <mallemajay@gmail.com>
2026-07-26 16:51:25 +0530

    Time: 59 ms (49.50%) | Memory: 111.3 MB (31.37%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -2,10 +2,37 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int, int> freq;
-        for(int i: nums) {
-            if(freq[i] > 0) return true;
+        for (int i : nums) {
+            if (freq[i] > 0)
+                return true;
             freq[i]++;
         }
         return false;
     }
 };
+
+/*
+Feedback -unordered_set (Best)
+bool containsDuplicate(vector<int>& nums) {
+    unordered_set<int> seen;
+
+    for (int num : nums) {
+        if (seen.count(num))
+            return true;
+        seen.insert(num);
+    }
+
+    return false;
+}
+Why?
+
+A set is exactly what you need:
+
+Have I seen this number before? ✅
+No need to store frequencies.
+
+Complexity
+
+Time: O(n) average
+Space: O(n)
+*/
