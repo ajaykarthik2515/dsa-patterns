ab752c0e466c4140d0e1edc7ce5f015645ae4872
ajaykarthik2515 <mallemajay@gmail.com>
2026-07-26 23:46:54 +0530

    Time: 38 ms (75.46%) | Memory: 45.5 MB (56.28%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,6 +2,25 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n=nums.size();
+        unordered_map<int, int> mp;
+        mp[0]=1;
+        int prefixSum=0;
+        int ans=0;
+        for(int num: nums){
+            prefixSum+=num;
+            if(mp.find(prefixSum-k)!=mp.end()){
+                ans+=mp[prefixSum-k];
+            }
+            mp[prefixSum]++;
+        }
+        return ans;
+        
+    }
+};
+/*
+above one is optimised and here is the brute fore approach
+Brute force -
+int n=nums.size();
         int ans=0;
         int sum;
         for(int i=0;i<n;i++){
@@ -12,6 +31,4 @@ public:
             }
         }
         return ans;
-        
-    }
-};
+*/
