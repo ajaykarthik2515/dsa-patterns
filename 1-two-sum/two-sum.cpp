15e6cb7ddbec7812b6084a8d60a4e09f2498aeb9
ajaykarthik2515 <mallemajay@gmail.com>
2026-07-26 17:06:45 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (39.23%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,19 +1,41 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       vector<int> result;
-       unordered_map<int, int> mp;
-       for(int i=0;i<nums.size();i++){
-        if(mp.find(target-nums[i])!=mp.end()){
-            int firstIndex = target-nums[i];
-            result.push_back(mp[firstIndex]);
-            result.push_back(i);
-            
-        } else {
-            mp[nums[i]]= i;
+        vector<int> res;
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            int remainingSum = target-nums[i];
+            if(mp.find(remainingSum)!=mp.end()){
+                res.push_back(mp[remainingSum]);
+                res.push_back(i);
+                return res;
+            }
+            mp[nums[i]]=i;
+        }
+        return res;
+       
+    }
+
+/*
+Cleaner version
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+
+        if (mp.find(complement) != mp.end()) {
+            return {mp[complement], i};
         }
 
-       }
-       return result;
+        mp[nums[i]] = i;
     }
+
+    return {};
+}
+Improvements over yours
+Uses a more descriptive variable name (complement instead of remainingSum).
+Returns {mp[complement], i} directly instead of creating a result vector and pushing elements.
+Returns {} instead of an empty vector variable.
+*/
 };
