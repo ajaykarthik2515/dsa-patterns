582408ecb9861542ab6e6ff9f012bff05f9bf4eb
ajaykarthik2515 <mallemajay@gmail.com>
2026-07-30 00:06:33 +0530

    Time: 23 ms (66.34%) | Memory: 88.3 MB (44.25%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -14,7 +14,6 @@ public:
         for(int i=0;i<n;i++)
         {
             prefixSum+=nums[i];
-            
             if(mp.find(prefixSum)==mp.end()){
                 mp[prefixSum]=i;
             }else{
@@ -24,3 +23,28 @@ public:
         return ans;
     }
 };
+
+/*
+One small improvement: you don't need the first loop or need to modify the input array. Do the conversion while calculating the prefix sum:
+
+int findMaxLength(vector<int>& nums) {
+    unordered_map<int, int> mp;
+    mp[0] = -1;
+
+    int prefixSum = 0;
+    int ans = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+        prefixSum += (nums[i] == 0) ? -1 : 1;
+
+        if (mp.find(prefixSum) == mp.end()) {
+            mp[prefixSum] = i;
+        } else {
+            ans = max(ans, i - mp[prefixSum]);
+        }
+    }
+
+    return ans;
+}
+*/
+
